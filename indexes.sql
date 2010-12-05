79666dc9f431da90ab6350855d1076335e2aaad3
Nick Gerner <nick.gerner@gmail.com>
2010-12-05 09:13:43 -0800

    adding sql files with suggested schema and indexes for analysis

---
+++ indexes.sql
@@ -0,0 +1,8 @@
+CREATE INDEX cid ON classes (id);
+CREATE INDEX iid ON instances (id);
+CREATE INDEX icid ON instances (classId);
+CREATE INDEX riid ON refs (id);
+CREATE INDEX rcid ON refs (classId);
+CREATE INDEX rrid ON refs (refId);
+
+

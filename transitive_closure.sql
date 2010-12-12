f554f1099377f54ed3a54b30c40050c98ec002fd
Nick Gerner <nick.gerner@gmail.com>
2010-12-12 09:46:07 -0800

    more efficient transitive closure script

--- transitive_closure.sql
+++ transitive_closure.sql
@@ -1,4 +1,10 @@
 BEGIN;
-INSERT INTO closure SELECT refs.refId FROM closure JOIN refs ON closure.id = refs.id;
-SELECT COUNT(*) FROM closure;
+DELETE FROM cd2;
+INSERT INTO cd2 SELECT DISTINCT refs.refId FROM closure_delta JOIN refs ON closure_delta.id = refs.id LEFT OUTER JOIN closure ON refs.refId = closure.id WHERE closure.id IS NULL;
+DELETE FROM closure_delta;
+INSERT INTO closure SELECT id FROM cd2;
+INSERT INTO closure_delta SELECT id FROM cd2;
+SELECT COUNT(*) FROM closure_delta;
+select classes.id, name, COUNT(closure_delta.id) FROM closure_delta join instances on closure_delta.id=instances.id join classes on instances.classId = classes.id GROUP BY classes.id, name ORDER BY COUNT(closure_delta.id) DESC LIMIT 10;
 COMMIT;
+

a86c6f65d0b6acca37b99a81ae1986051d1fa31b
Nick Gerner <nick.gerner@gmail.com>
2010-12-28 18:47:23 -0800

    refactoring transitive closure to be a bit faster

--- reverse_transitive_closure.sql
+++ reverse_transitive_closure.sql
@@ -1,4 +1,11 @@
+CREATE TEMP TABLE cd2 (id INTEGER);
 BEGIN;
-INSERT INTO closure SELECT refs.id FROM closure JOIN refs ON closure.id = refs.refId;
-SELECT COUNT(*) FROM closure;
+INSERT INTO cd2 SELECT DISTINCT refs.id FROM closure_delta JOIN refs ON closure_delta.id = refs.refId LEFT OUTER JOIN closure ON refs.id = closure.id WHERE closure.id IS NULL;
+DELETE FROM closure_delta;
+INSERT INTO closure SELECT id FROM cd2;
+INSERT INTO closure_delta SELECT id FROM cd2;
+SELECT COUNT(*) FROM closure_delta;
+select classes.id, name, COUNT(closure_delta.id), SUM(instances.size) FROM closure_delta join instances on closure_delta.id=instances.id join classes on instances.classId = classes.id GROUP BY classes.id, name ORDER BY COUNT(closure_delta.id) DESC LIMIT 10;
 COMMIT;
+DROP TABLE cd2;
+
--- transitive_closure.sql
+++ transitive_closure.sql
@@ -1,10 +1,13 @@
+SELECT DATETIME();
+CREATE TEMP TABLE cd2 (id INTEGER);
 BEGIN;
-DELETE FROM cd2;
 INSERT INTO cd2 SELECT DISTINCT refs.refId FROM closure_delta JOIN refs ON closure_delta.id = refs.id LEFT OUTER JOIN closure ON refs.refId = closure.id WHERE closure.id IS NULL;
 DELETE FROM closure_delta;
-INSERT INTO closure SELECT id FROM cd2;
-INSERT INTO closure_delta SELECT id FROM cd2;
+INSERT INTO closure SELECT id FROM cd2 ORDER BY id;
+INSERT INTO closure_delta SELECT id FROM cd2 ORDER BY id;
 SELECT COUNT(*) FROM closure_delta;
-select classes.id, name, COUNT(closure_delta.id) FROM closure_delta join instances on closure_delta.id=instances.id join classes on instances.classId = classes.id GROUP BY classes.id, name ORDER BY COUNT(closure_delta.id) DESC LIMIT 10;
+select classes.id, name, COUNT(closure_delta.id), SUM(instances.size) FROM closure_delta join instances on closure_delta.id=instances.id join classes on instances.classId = classes.id GROUP BY classes.id, name ORDER BY COUNT(closure_delta.id) DESC LIMIT 20;
 COMMIT;
+DROP TABLE cd2;
+SELECT DATETIME();
 

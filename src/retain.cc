5c4c9554341c2b7cbf15804e359565cbf2276d8d
Nick Gerner <nick.gerner@gmail.com>
2011-01-25 09:21:53 -0800

    adding column of count of objects retained

--- src/retain.cc
+++ src/retain.cc
@@ -26,6 +26,8 @@ int main(int argc, char **argv) {
 	numNodes = sb.st_size/sizeof(u64);
 	int retainedFd = openAndInitializeFile(argv[4], numNodes*sizeof(u64));
 	assert(retainedFd >= 0);
+	int retainedCountFd = openAndInitializeFile(argv[5], numNodes*sizeof(u64));
+	assert(retainedCountFd >= 0);
 
 	//mmap size, retained
 	u64 *vertex = (u64 *)mmap(NULL, numNodes*sizeof(u64), PROT_READ, MAP_PRIVATE, vertexFd, 0);
@@ -36,9 +38,12 @@ int main(int argc, char **argv) {
 	assert(size != MAP_FAILED);
 	u64 *retained = (unsigned long long *)mmap(NULL, numNodes*sizeof(unsigned long long), PROT_READ | PROT_WRITE, MAP_SHARED, retainedFd, 0);
 	assert(retained != MAP_FAILED);
+	u64 *retainedCount = (unsigned long long *)mmap(NULL, numNodes*sizeof(unsigned long long), PROT_READ | PROT_WRITE, MAP_SHARED, retainedCountFd, 0);
+	assert(retainedCount != MAP_FAILED);
 
 	//intialize retained to the size of each instance
 	bzero(retained, numNodes*sizeof(u64));
+	bzero(retainedCount, numNodes*sizeof(u64));
 
 	u64 skipped = 0;
 	u64 totalSize = 0;
@@ -56,14 +61,16 @@ int main(int argc, char **argv) {
 			assert(dominator[v] < numNodes);
 			totalSize += size[v];
 			retained[v] += size[v];
-			//TODO: need to add this size to dominator's dominator and so on...
 			retained[dominator[v]] += retained[v];
+			retainedCount[v] += 1;
+			retainedCount[dominator[v]] += retainedCount[v];
 		} else {
 			skipped++;
 		}
 		sizeIncludingUnreachable += size[i];
 	}
 	assert(totalSize == retained[0]);
+	assert(numNodes-skipped == retainedCount[0]+1);
 	fprintf(stderr, "\n%llu retained in %llu reachable nodes, plus %llu unreachable in %llu unreachable nodes\n", retained[0], numNodes-skipped, sizeIncludingUnreachable - totalSize, skipped);
 
 	retval = munmap(vertex, numNodes*sizeof(unsigned long long));
@@ -74,11 +81,14 @@ int main(int argc, char **argv) {
 	assert(retval != -1);
 	retval = munmap(retained, numNodes*sizeof(unsigned long long));
 	assert(retval != -1);
+	retval = munmap(retainedCount, numNodes*sizeof(unsigned long long));
+	assert(retval != -1);
 	
 	close(vertexFd);
 	close(dominatorFd);
 	close(sizeFd);
 	close(retainedFd);
+	close(retainedCountFd);
 	
 	return 0;
 }
